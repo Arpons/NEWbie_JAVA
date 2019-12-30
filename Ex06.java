421c21564c036fbf167f47aff3bdb568baa99bf4
Arpons <49786729+Arpons@users.noreply.github.com>
2019-12-30 11:07:13 +0900

    2019.12.30

--- Ex06.java
+++ Ex06.java
@@ -1,9 +1,33 @@
+package ee;
+
+import java.util.Scanner;
 
 public class Ex06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		 int money = 0; // 잔돈
+		 int cnt = 0; // 각 화폐의 개수
+		 // 각 화폐들
+		 int[] coins = {10000, 5000, 1000, 500, 100};
+		 // 배열 생성, 배열의 이름은 복수로 작성
+		
+		 int a = 5;
+		 int b = a++;
+		 System.out.println(a);
+		 System.out.println(b);
+		 int c = ++a;
+		 System.out.println(c);
+		 System.out.println("[잔돈 계산기]");
+		 System.out.println("거스름 돈 >>> ");
+		 money = scan.nextInt();
+		 
+		 for(int i = 0; i < coins.length; i++) {
+			 cnt = money / coins[i];
+			 System.out.println(coins[i] + "원  개수 >>>" + cnt);
+			 money %= coins[i];
+		 }
+		 
 	}
 
 }
---
+++ Ex07.java
@@ -0,0 +1,13 @@
+package ee;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		int a = 3, b = 4, c = 5, d = 5;
+		a += 6 + --b;
+		d *= 7 - c++;
+		System.out.println(a + "," + b + "," + c + "," + d);
+
+	}
+
+}
