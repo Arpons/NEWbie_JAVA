b9e5580579a9e923fde9b24fad904f01186ef32a
Arpons <49786729+Arpons@users.noreply.github.com>
2019-12-30 11:51:28 +0900

    box

--- Ex07.java
+++ Ex07.java
@@ -1,12 +1,22 @@
 package ee;
 
+import java.util.Scanner;
+
 public class Ex07 {
 
 	public static void main(String[] args) {
-		int a = 3, b = 4, c = 5, d = 5;
-		a += 6 + --b;
-		d *= 7 - c++;
-		System.out.println(a + "," + b + "," + c + "," + d);
+		 
+		 Scanner scan = new Scanner(System.in);
+		 
+		 int ball = 0;	 
+		 int boxSize = 5;
+		 int result = 0;
+		 
+		 
+		 System.out.println("농구공의 개수를 입력하세요 : ");
+		 ball = scan.nextInt();
+		 result = ball % boxSize == 0 ?  ball / boxSize : ball /  boxSize + 1;
+		 System.out.println("필요한 상자 수: " +  result);
 
 	}
 
